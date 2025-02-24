45d6bd8e144e3336666b79dcda2e28da26a61184
Peter Hsu <peter.hsu@sparkful.com>
2025-02-24 12:53:58 +0800

    Initial Commit

---
+++ HelloSpeakBuddy/HelloSpeakBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSpeakBuddyApp.swift
+//  HelloSpeakBuddy
+//
+//  Created by Peter Hsu on 2025/2/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloSpeakBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloSpeakBuddyTests/HelloSpeakBuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSpeakBuddyTests.swift
+//  HelloSpeakBuddyTests
+//
+//  Created by Peter Hsu on 2025/2/24.
+//
+
+import Testing
+@testable import HelloSpeakBuddy
+
+struct HelloSpeakBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
