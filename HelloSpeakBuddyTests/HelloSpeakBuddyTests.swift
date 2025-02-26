f133ef2c820c73ce2bb9b03c19560fe46c9d206e
Peter Hsu <peter.hsu@sparkful.com>
2025-02-26 14:11:06 +0800

    Add demo UT cases for SubscriptionViewModel

--- HelloSpeakBuddyTests/HelloSpeakBuddyTests.swift
+++ HelloSpeakBuddyTests/HelloSpeakBuddyTests.swift
@@ -8,10 +8,28 @@
 import Testing
 @testable import HelloSpeakBuddy
 
-struct HelloSpeakBuddyTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+struct SubsriptionViewModelTests {
+    
+    @Test("Initialization")
+    func test_initialization() async throws {
+        let viewModel = TestableSubscriptionViewModel()
+        #expect(!viewModel.showGraph)
     }
 
+    @Test("Register Plan")
+    func test_register_plan() async throws {
+        let viewModel = TestableSubscriptionViewModel()
+        viewModel.registerPlan()
+        #expect(viewModel.registerPlanCalled)
+        // Other assertions about real registeration logic
+        // or did call the function of the other service, such as SubscriptionManager
+    }
+    
+    @Test("Dismiss View")
+    func test_dismiss_view() async throws {
+        let viewModel = TestableSubscriptionViewModel()
+        viewModel.dismiss()
+        #expect(viewModel.dismissCalled)
+        // Other assertions about dismissing view logic
+    }
 }
